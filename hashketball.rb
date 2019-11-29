c56515c5086f7f4b5ee0e30480b7c32b0457fb49
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:17:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,63 +4,63 @@ def game_hash
   game_hash = {
     home: {
       team_name:"",
-      colors:[], 
-      players: 
+      colors:[],
+      players:
       [
         {
-          player_name: "", 
-          number: 0, 
-          shoe: 0, 
-          points: 0, 
-          rebounds: 0, 
-          assists: 0, 
-          steals: 0, 
-          blocks: 0, 
-          slam_dunks: 0 
+          player_name: "",
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks: 0
         },
           {
-            player_name: "", 
-            number: 0, 
-            shoe: 0, 
-            points: 0, 
-            rebounds: 0, 
-            assists: 0, 
-            steals: 0, 
-            blocks: 0, 
-            slam_dunks: 0 
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
           },
           {
-            player_name: "", 
-            number: 0, 
-            shoe: 0, 
-            points: 0, 
-            rebounds: 0, 
-            assists: 0, 
-            steals: 0, 
-            blocks: 0, 
-            slam_dunks: 0 
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
           },
           {
-            player_name: "", 
-            number: 0, 
-            shoe: 0, 
-            points: 0, 
-            rebounds: 0, 
-            assists: 0, 
-            steals: 0, 
-            blocks: 0, 
-            slam_dunks: 0 
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
           },
           {
-            player_name: "", 
-            number: 0, 
-            shoe: 0, 
-            points: 0, 
-            rebounds: 0, 
-            assists: 0, 
-            steals: 0, 
-            blocks: 0, 
-            slam_dunks: 0 
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
           }
         ]
     },
