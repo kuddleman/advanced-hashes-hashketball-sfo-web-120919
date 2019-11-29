e65269c593fd2d6be78240c6ecfcfd609ea6ff2f
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:30:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,15 +70,15 @@ def game_hash
         players:
         [
           {
-            player_name: "",
-            number: 0,
-            shoe: 0,
-            points: 0,
-            rebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
-            slam_dunks: 0
+            player_name: "Jeff Adrien",
+            number: 4,
+            shoe: 18,
+            points: 10,
+            rebounds: 1,
+            assists: 1,
+            steals: 2,
+            blocks: 7,
+            slam_dunks: 2
           },
           {
             player_name: "",
