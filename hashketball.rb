b01fb9adf22c8748c9b7c40cb0449395029116b0
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:06:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@ require 'awesome_print'
 
 def game_hash
   game_hash = {
-    home: {team_name:{},colors:{}, players: {}},
-    away: {team_name:{},colors:{}, players: {}}
+    home: {team_name:"",colors:[], players: [{},{},{},{},{}]},
+    away: {team_name:"",colors:[], players: [{},{},{},{},{}]}
   }
 
 end
