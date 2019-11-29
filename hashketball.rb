48367506f4d2f65be1649a68ef2ce7527d1fc0c3
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:33:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,29 +81,29 @@ def game_hash
             slam_dunks: 2
           },
           {
-            player_name: "",
+            player_name: "Bismack Biyombo",
             number: 0,
-            shoe: 0,
-            points: 0,
-            rebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
-            slam_dunks: 0
+            shoe: 16,
+            points: 12,
+            rebounds: 4,
+            assists: 7,
+            steals: 22,
+            blocks: 15,
+            slam_dunks: 10
           },
           {
-            player_name: "",
-            number: 0,
-            shoe: 0,
-            points: 0,
-            rebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
-            slam_dunks: 0
+            player_name: "DeSagna Diop",
+            number: 2,
+            shoe: 14,
+            points: 24,
+            rebounds: 12,
+            assists: 12,
+            steals: 4,
+            blocks: 5,
+            slam_dunks: 5
           },
           {
-            player_name: "",
+            player_name: "Ben Gordon",
             number: 0,
             shoe: 0,
             points: 0,
