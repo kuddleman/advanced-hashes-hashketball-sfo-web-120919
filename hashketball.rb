02d25a71799a48a19b70edef9d1b5d1ca629d2e8
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:22:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,68 @@ def game_hash
           }
         ]
     },
-    away: {}
+    away: {
+        team_name:"",
+        colors:[],
+        players:
+        [
+          {
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
+          },
+          {
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
+          },
+          {
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
+          },
+          {
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
+          },
+          {
+            player_name: "",
+            number: 0,
+            shoe: 0,
+            points: 0,
+            rebounds: 0,
+            assists: 0,
+            steals: 0,
+            blocks: 0,
+            slam_dunks: 0
+          }
+        ]
+    }
   }
 
 end
