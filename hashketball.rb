4916710ee865c536d8f37a2dac2425496dd6f729
Donald Lobree <edl1155@gmail.com>
2019-11-29 03:34:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -104,25 +104,25 @@ def game_hash
           },
           {
             player_name: "Ben Gordon",
-            number: 0,
-            shoe: 0,
-            points: 0,
-            rebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
+            number: 8,
+            shoe: 015,
+            points: 33,
+            rebounds: 3,
+            assists: 2,
+            steals: 1,
+            blocks: 1,
             slam_dunks: 0
           },
           {
-            player_name: "",
-            number: 0,
-            shoe: 0,
-            points: 0,
-            rebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
-            slam_dunks: 0
+            player_name: "Kemba Walker",
+            number: 33,
+            shoe: 15,
+            points: 6,
+            rebounds: 12,
+            assists: 12,
+            steals: 7,
+            blocks: 5,
+            slam_dunks: 12
           }
         ]
     }
